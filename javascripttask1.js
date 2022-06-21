15bbf2b0a80b37a9640b48b3d96baba14b1dc52d
Gail-ng <105196495+Gail-ng@users.noreply.github.com>
2022-06-21 23:31:46 +0100

    Add files via upload

---
+++ javascripttask1.js
@@ -0,0 +1,14 @@
+//creating a variable storing my personal data
+let person = {
+  name: 'My name is Abigail',
+  age: 'I am 32 years old',
+  country: 'a nigerian citizen',
+  height: 'with a height of 123cm',
+}
+
+const { name, age,country,height } = person; // Takes the property/method from the object
+
+console.log(name); // Prints 'My name is Abigail'
+console.log(age); // Prints 'i am 32 years old'
+console.log(country); // Prints 'a nigerian citizen'
+console.log(height); // Prints 'with a height of 123cm'
